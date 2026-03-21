8fd87e8b65d04c86b72a84f77679e59e536cd9e7
Abdelrahman Atef <man1207468@gmail.com>
2026-03-21 02:38:03 +0200

    Update entry points to support asynchronous initialization
    - Update `main` functions in `lib/main_dev.dart` and `lib/main_prod.dart` to return `Future<void>`.
    - Use `await` when calling `mainCommon()` in both development and production entry points.

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,12 +1,12 @@
 import 'package:habits10/core/config/flavor_config.dart';
 import 'package:habits10/main_common.dart';
 
-void main() {
+Future<void> main() async {
   FlavorConfig(
     flavor: Flavor.DEV,
     name: "DEV",
     apiBaseUrl: "https://dev-api.example.com", // Replace with actual Dev API
     appTitle: "10Habits Dev",
   );
-  mainCommon();
+  await mainCommon();
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,12 +1,12 @@
 import 'package:habits10/core/config/flavor_config.dart';
 import 'package:habits10/main_common.dart';
 
-void main() {
+Future<void> main() async {
   FlavorConfig(
     flavor: Flavor.PROD,
     name: "PROD",
     apiBaseUrl: "https://api.example.com", // Replace with actual Prod API
     appTitle: "10Habits",
   );
-  mainCommon();
+  await mainCommon();
 }
